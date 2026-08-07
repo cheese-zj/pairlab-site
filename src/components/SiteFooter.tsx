78f0f39f9a97ae33bb98fac5cee19294c0445e6d
James <jzha0385@uni.sydney.edu.au>
2026-08-07 17:01:01 +1000

    Refine homepage layout on wide screens

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -6,7 +6,6 @@ function SiteFooter() {
       <Link to="/">PAIR Lab</Link>
       <p>School of Computer Science · The University of Sydney</p>
       <div>
-        <a href="https://simontruffer.ch/mt-shift/" target="_blank" rel="noreferrer">MT Shift — Simon Truffer</a>
         <a href="https://www.weimingzhi.com/publication-list" target="_blank" rel="noreferrer">Publications</a>
         <a href="mailto:pairlab212@gmail.com">Email</a>
       </div>
