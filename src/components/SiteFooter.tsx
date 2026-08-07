33374e65fb286d712cb2f2027b6e5392b13c13f5
James <jzha0385@uni.sydney.edu.au>
2026-08-07 17:17:13 +1000

    Refine routed page layout and typography

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 function SiteFooter() {
   return (
     <footer className="site-footer">
-      <Link to="/">PAIR Lab</Link>
-      <p>School of Computer Science · The University of Sydney</p>
-      <div>
-        <a href="https://www.weimingzhi.com/publication-list" target="_blank" rel="noreferrer">Publications</a>
-        <a href="mailto:pairlab212@gmail.com">Email</a>
+      <div className="site-footer-inner">
+        <Link to="/">PAIR Lab</Link>
+        <p>School of Computer Science · The University of Sydney</p>
+        <div className="site-footer-links">
+          <a href="https://www.weimingzhi.com/publication-list" target="_blank" rel="noreferrer">Publications</a>
+          <a href="mailto:pairlab212@gmail.com">Email</a>
+        </div>
+        <span>© {new Date().getFullYear()}</span>
       </div>
-      <span>© {new Date().getFullYear()}</span>
     </footer>
   )
 }
